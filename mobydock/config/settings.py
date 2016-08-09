2b721321b0b23b0c29f1f037689fc6d5d37e8686
Fernando Garcia <odnanref@kubrick-pro-2.local>
2016-08-08 21:36:28 -0700

    initial setup

---
+++ mobydock/config/settings.py
@@ -0,0 +1,9 @@
+SECRET_KEY = 'iloveb33r'
+
+APP_NAME = 'Moby Dock'
+
+DEBUG = True
+TESTING = False
+
+SQLALCHEMY_DATABASE_URI = 'postgresql://mobydock:iloveb33r@postgres:5432/mobydock'
+REDIS_URL = 'redis://redis:6379/0'
